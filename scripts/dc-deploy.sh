10d4e1ccf412b06ad7cbbbf61d3655b97bcd933e
harry hayes <harry.hayes@holidayextras.com>
2024-05-24 15:40:58 +0100

    fixed some shitty code

--- scripts/dc-deploy.sh
+++ scripts/dc-deploy.sh
@@ -5,11 +5,13 @@ containers_up=$(sudo docker-compose ps | grep "Up")
 
 if [ -z "$containers_up" ]; then
     echo "Containers are not running. Starting them with 'up -d' command."
+    sudo docker system prune -f
     sudo docker-compose build
     sudo docker-compose up -d
 else
     echo "Containers are running. Restarting them with 'restart' command."
     sudo docker-compose down -v
+     sudo docker system prune -f
     sudo docker-compose build
     sudo docker-compose up -d
 fi
