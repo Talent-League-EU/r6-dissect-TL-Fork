324cf652cc6f144f5a04b2a645b4396063f184bf
harry hayes <harry.hayes@holidayextras.com>
2024-05-24 20:47:26 +0100

    fixed some shitty code

--- scripts/autorun.sh
+++ scripts/autorun.sh
@@ -8,3 +8,9 @@ sleep 600
 
 # Second POST request to port 5002
 curl -X POST http://35.177.150.58:5002/api/runner
+
+# Sleep for 1 minutes (60 seconds)
+sleep 60
+
+# Third POST request to port 5003
+curl -X POST http://35.177.150.58:5003/api/runner
