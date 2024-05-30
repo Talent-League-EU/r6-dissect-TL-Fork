752b0ac16bd0e0494f7eeb8d287c0bd62a7de1a4
harry hayes <harry.hayes@holidayextras.com>
2024-05-31 00:37:20 +0100

    updated file upload locales

--- file-upload-app/public/js/app.js
+++ file-upload-app/public/js/app.js
@@ -1,10 +1,13 @@
-// public/js/app.js
 $(document).ready(function() {
-    $('#test-all').click(function() {
-      $.post('/api/endpoint', function(data) {
-        $('#responseBody').text(data);
-        $('#responseModal').modal('show');
-      });
+    i18next.init({
+      lng: 'en',  // default language
+      backend: {
+        loadPath: '/locales/{{lng}}/translation.json'
+      }
+    }, function(err, t) {
+      if (err) return console.error('i18next initialization error:', err);
+      jqueryI18next.init(i18next, $);
+      $('body').localize(); // This localizes the body after i18next is ready
     });
-  });
+});
   
