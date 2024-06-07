800fb628c1cb81cdbb5d036fd498275d645198fc
harry hayes <harry.hayes@holidayextras.com>
2024-06-07 16:56:19 +0100

    removed r6-dissect from repo, changed it to download in the deploy scripts instead

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,11 +6,12 @@ cd /etc/TLMRIS
 # Check if the directory change was successful
 if [ $? -eq 0 ]; then
     # Execute the go build command with sudo
-    git clone https://github.com/redraskal/r6-dissect.git
+    sudo git clone https://github.com/redraskal/r6-dissect.git
     cd r6-dissect
     sudo go build
     cd ..
-    mv r6-dissect/r6-dissect /dissect-runner
+    sudo mv r6-dissect/r6-dissect /dissect-runner/r6-dissect
+    sudo rm -rf r6-dissect
 else
     echo "Failed to change directory to /etc/TLMRIS. The directory may not exist."
 fi
